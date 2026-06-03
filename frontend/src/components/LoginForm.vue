f4b8db96bb5cc7514762b313e0fb12f43be9a21a
Brian Daza <bdaza3@uic.edu>
2026-06-02 20:08:50 -0500

    feat: initial login forms visibility

---
+++ frontend/src/components/LoginForm.vue
@@ -0,0 +1,32 @@
+<script>
+export default {
+  data() {
+    return {
+      username: '',
+      password: ''
+    }
+  },
+  methods: {
+    handleLogin() {
+        console.log('Logging in with', this.username, this.password)
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="login-container">
+    <h1>Login</h1>
+    <form @submit.prevent="handleLogin">
+      <div class="form-group">
+        <label for="username">Username:</label>
+        <input type="text" id="username" v-model="username" required />
+        </div>
+        <div class="form-group">
+        <label for="password">Password:</label>
+        <input type="password" id="password" v-model="password" required />
+        </div>
+        <button type="submit">Login</button>
+    </form>
+    </div>
+</template>
